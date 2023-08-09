2414107b951c3f629eafde45039566e6d4b00379
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2023-08-09 07:18:40 -0300

    implementação de login com google e manipulações do usuario]

---
+++ evento/resources/views/Home/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout title="Home">
+    <a href="{{route('usuario.index')}}" >
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="" alt="Imagem de capa do card">
+            <div class="card-body">
+                <h5 href="{{route('usuario.index')}}" class="card-title">Usuarios</h5>
+            </div>
+        </div>
+    </a>
+
+
+</x-layout>
--- evento/routes/web.php
+++ evento/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\HomeController;
+use App\Http\Middleware\Autenticador;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SocialiteController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +13,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [HomeController::class, 'index'])->name('home.index')->secure();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//home
+Route::get('/home', [HomeController::class, 'index'])->name('home.index')->secure();
+
+//usuarios
+Route::resource('/usuario', UsuarioController::class)->middleware(Autenticador::class);
+
+
+//Route::get('login/google', "SocialiteController@redirectToProvider");
+//Route::get('login/google/callback', 'SocialiteController@handleProviderCalback');
+Route::get('login/google', [SocialiteController::class, 'redirectToProvider'])->name('login');
+Route::get('login/google/callback', [SocialiteController::class, 'hendProviderCallback']);
+Route::get('login/logout', [SocialiteController::class, 'destroy'])->name('logout');
